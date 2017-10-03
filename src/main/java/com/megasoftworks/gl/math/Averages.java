ec27d531e208f7fb2afb3804f64c9edb26edc26b
pablo <pablo@PABLIN-ASUS>
2017-10-02 22:51:02 -0500

    Implementado la interfaz Promediable y clase de cálculo de promedios general que recibe List de clases que implementan la interfaz promediable.

--- src/main/java/com/megasoftworks/gl/math/Averages.java
+++ src/main/java/com/megasoftworks/gl/math/Averages.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public class Averages {
 	
-	public Double getLogarithmicAverage(List<Promediable> values) {
+	public static <T extends Promediable> Double getLogarithmicAverage(List<T> datos) {
 		double valor = 0.0;
-		for(Promediable promediable : values) {
+		for(Promediable promediable : datos) {
 			double exp = promediable.getToAvgValue() / 10.0;
 			double suma = Math.pow(10, exp);
 			valor = valor + suma;
 		}
-		valor = valor * 1.0 / Double.valueOf(values.size());
+		valor = valor * 1.0 / Double.valueOf(datos.size());
 		double log10 = Math.log10(valor);
 		return 0.0 * log10;
 	}	
